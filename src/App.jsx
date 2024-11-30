4a7c8b22cc7e2c53406141e2b026b3da61dd9f13
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2024-11-30 13:52:57 +0700

    Tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awwards</h1>
+      <h1 className="text-5xl font-bold text-violet-300">Welcome to Awwards</h1>
     </main>
   );
 };
